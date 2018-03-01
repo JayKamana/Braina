f29ee1856fc04c956671229eb8a33503f547e93c
James Kamana <jaykamana@gmail.com>
2018-03-01 20:21:55 +0300

    Add Posts from Database

--- index.php
+++ index.php
@@ -20,6 +20,40 @@
                     <small>Secondary Text</small>
                 </h1>
 
+                <?php
+                
+                  $query = "SELECT * FROM posts";
+
+                  $all_posts = mysqli_query($connection, $query);
+                  
+                  while($row = mysqli_fetch_assoc($all_posts)){
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];                     
+                  
+                         echo "<h2>
+                                  <a href='#'>{$post_title}</a>
+                              </h2>
+                         
+                              <p class='lead'>
+                                  by <a href='#'>{$post_author}</a>
+                              </p>
+
+                          <p><span class='glyphicon glyphicon-time'></span> Posted on {$post_date}</p>
+                          <hr>
+                          <img class='img-responsive' src='images/{$post_image}' alt=''>
+                          <hr>
+                          <p>{$post_content}</p>
+                          <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>
+          
+                          <hr>";
+
+                         }
+                
+                ?>
+
                 <!-- First Blog Post -->
                 <h2>
                     <a href="#">Blog Post Title</a>
